0301e58b8a74e7661bee8830f354552980e856f4
khanhln1995 <lenguyen.khanh1995@gmail.com>
2025-08-19 22:49:05 +0700

    ok

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="bg-yellow-400 text-white m-5 p-5 text-3xl font-bold border-purple-500 border-3 text-center ">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
